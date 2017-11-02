88927650b24f2e7e054517be28ef9485b1ec6b1f
Yuliyan <y.georgiev@student.tue.nl>
2017-11-02 13:21:02 +0100

    Added Press R to poop message

---
+++ GameDesignProject/Assets/Scripts/PoopMessage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PoopMessage : MonoBehaviour {
+
+    public Text poopByR;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            poopByR.text = "Press R to poop.";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        poopByR.text = "";
+        Destroy(gameObject);
+    }
+}
